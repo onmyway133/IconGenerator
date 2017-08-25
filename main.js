32257ec7f5bb0aae5c19d38e4cd4e8428b66dbd1
Khoa Pham <onmyway133@gmail.com>
2017-08-25 14:14:53 +0200

    Add renderer.js

---
+++ main.js
@@ -0,0 +1,40 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+const Menu = require('electron').Menu
+
+// global
+let win
+
+function createWindow () {
+   win = new BrowserWindow({
+    title: 'Push Notifications Tester',
+    width: 800, 
+    height: 700,
+    icon: __dirname + '/Icon/Icon.icns'
+  })
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
