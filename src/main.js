48d6047a139da91938b5b29209922a37fa47ae26
Khoa Pham <onmyway133@gmail.com>
2018-08-16 13:04:34 +0200

    Add source files to src

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ function createWindow () {
   })
 
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '../index.html'),
     protocol: 'file:',
     slashes: true
   }))
