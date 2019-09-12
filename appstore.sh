713b84e18b692d9e8dbc60904c1b4e9d03f2aa77
Khoa Pham <onmyway133@gmail.com>
2019-09-12 21:21:34 +0200

    Update

---
+++ appstore.sh
@@ -0,0 +1,3 @@
+npx electron-packager . "IconGenerator" --app-bundle-id=com.onmyway133.IconGenerator --helper-bundle-id=com.onmyway133.IconGenerator.helper --app-version=1.3.0 --build-version=2 --platform=mas --arch=x64 --icon=Icon/Icon.icns --overwrite
+npx electron-osx-sign "IconGenerator-mas-x64/IconGenerator.app" --verbose
+npx electron-osx-flat "IconGenerator-mas-x64/IconGenerator.app" --verbose
