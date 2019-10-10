03f7de7b69a340eaeefdbd93c73af62f858353d4
Khoa Pham <onmyway133@gmail.com>
2019-10-10 22:25:09 +0200

    Update profile

--- appstore.sh
+++ appstore.sh
@@ -1,3 +1,3 @@
-npx electron-packager . "IconGenerator" --app-bundle-id=com.onmyway133.IconGenerator --helper-bundle-id=com.onmyway133.IconGenerator.helper --app-version=1.4.0 --build-version=5 --platform=mas --arch=x64 --icon=Icon/Icon.icns --overwrite
+npx electron-packager . "IconGenerator" --app-bundle-id=com.onmyway133.IconGenerator --helper-bundle-id=com.onmyway133.IconGenerator.helper --app-version=1.4.0 --build-version=8 --platform=mas --arch=x64 --icon=Icon/Icon.icns --overwrite
 npx electron-osx-sign "IconGenerator-mas-x64/IconGenerator.app" --verbose
 npx electron-osx-flat "IconGenerator-mas-x64/IconGenerator.app" --verbose
